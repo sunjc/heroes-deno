24d7bed09bda54a0ff91ba4e0d7e6d991109edea
Sun Jingchuan <sjc-925@163.com>
2021-01-18 21:07:41 +0800

    upgrade to deno 0.81.0

--- deps.ts
+++ deps.ts
@@ -1,37 +1,37 @@
 export {
-  green,
-  cyan,
   bold,
+  cyan,
+  green,
   yellow,
-} from "https://deno.land/std@0.74.0/fmt/colors.ts";
+} from "https://deno.land/std@0.81.0/fmt/colors.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.1/mod.ts";
 export {
-  makeJwt,
-  setExpiration,
-  Jose,
-  Payload,
-} from "https://deno.land/x/djwt@v1.7/create.ts";
-export { validateJwt } from "https://deno.land/x/djwt@v1.7/validate.ts";
-export { config } from "https://deno.land/x/dotenv@v0.5.0/mod.ts";
+  create,
+  getNumericDate,
+  verify,
+} from "https://deno.land/x/djwt@v2.0/mod.ts";
+export type { Header, Payload } from "https://deno.land/x/djwt@v2.0/mod.ts";
+export { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
 export {
   Application,
   Context,
   helpers as oakHelpers,
   HttpError,
   httpErrors,
-  HTTPMethods,
   isHttpError,
   Request,
   Response,
   Router,
-  RouterContext,
-  RouteParams,
   Status,
-} from "https://deno.land/x/oak@v6.3.1/mod.ts";
+} from "https://deno.land/x/oak@v6.4.1/mod.ts";
+export type {
+  HTTPMethods,
+  RouteParams,
+  RouterContext,
+} from "https://deno.land/x/oak@v6.4.1/mod.ts";
 export {
   Column,
-  ConnectionOptions,
   ConnectionOptionsReader,
   createConnection,
   CreateDateColumn,
@@ -43,30 +43,34 @@ export {
   getRepository,
   JoinTable,
   ManyToMany,
-  MigrationInterface,
   PrimaryGeneratedColumn,
   Repository,
-  QueryRunner,
   UpdateDateColumn,
+} from "https://raw.githubusercontent.com/denolib/typeorm/master/mod.ts";
+export type {
+  ConnectionOptions,
+  MigrationInterface,
+  QueryRunner,
 } from "https://raw.githubusercontent.com/denolib/typeorm/master/mod.ts";
 export { Snelm } from "https://deno.land/x/snelm@1.3.0/mod.ts";
 export {
   firstMessages,
-  InputData,
   isEmail,
   lengthBetween,
   match,
   required,
   validate,
+} from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
+export type {
+  InputData,
   ValidationErrors,
   ValidationOptions,
   ValidationRules,
 } from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
-
 export {
   assert,
   assertEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.74.0/testing/asserts.ts";
-export { superoak } from "https://deno.land/x/superoak@2.3.1/mod.ts";
+} from "https://deno.land/std@0.81.0/testing/asserts.ts";
+// export {superoak} from "https://deno.land/x/superoak@3.0.1/mod.ts";
--- util/jwt.ts
+++ util/jwt.ts
@@ -1,4 +1,5 @@
-import {Jose, makeJwt, Payload, setExpiration, validateJwt} from "../deps.ts";
+import type {Header, Payload} from "../deps.ts";
+import {create, getNumericDate, verify} from "../deps.ts";
 import {config} from "../config/config.ts";
 
 const {
@@ -8,8 +9,8 @@ const {
     JWT_ISSUER,
 } = config;
 
-const header: Jose = {
-    alg: "HS256",
+const header: Header = {
+    alg: "HS512",
     typ: "JWT",
 };
 
@@ -21,27 +22,24 @@ async function makeAccessToken(user: any): Promise<string> {
         authorities: user.authorities.map((auth: { name: string }) => {
             return auth.name;
         }),
-        exp: setExpiration(parseInt(JWT_ACCESS_TOKEN_EXP)),
+        exp: getNumericDate(parseInt(JWT_ACCESS_TOKEN_EXP)),
     };
 
-    return await makeJwt({key: JWT_SECRET, header, payload});
+    return await create(header, payload, JWT_SECRET);
 }
 
 async function makeRefreshToken(user: any): Promise<string> {
     const payload: Payload = {
         iss: JWT_ISSUER,
         sub: user.username,
-        exp: setExpiration(parseInt(JWT_REFRESH_TOKEN_EXP)),
+        exp: getNumericDate(parseInt(JWT_REFRESH_TOKEN_EXP)),
     };
 
-    return makeJwt({key: JWT_SECRET, header, payload});
+    return await create(header, payload, JWT_SECRET);
 }
 
 async function getPayload(jwt: string): Promise<Payload | null> {
-    const jwtValidation = await validateJwt(
-        {jwt, key: JWT_SECRET, algorithm: "HS256"}
-    );
-    return jwtValidation.isValid ? jwtValidation.payload : null;
+    return await verify(jwt, JWT_SECRET, "HS512");
 }
 
 export {makeAccessToken, makeRefreshToken, getPayload};
